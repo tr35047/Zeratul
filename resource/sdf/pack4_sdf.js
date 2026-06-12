bd569db1978fb4cca19bb1ead7d33d47ee346602
tr35047 <tr35047@outlook.com>
2026-06-12 20:09:07 +0800

    V3.0.5 更新日志 时代服卡牌价值变更

--- resource/sdf/pack4_sdf.js
+++ resource/sdf/pack4_sdf.js
@@ -2,6 +2,6 @@
 window._packData["pack4_sdf"] = {"name":"穷兵黩武","cards":[
 {"id":"灵能发射器","race":"Terran","level":6,"number":3,"value":1000},
 {"id":"英灵殿","race":"Protess","level":6,"number":1,"value":1200},
-{"id":"虫群近卫","race":"Zerg","level":6,"number":7,"value":2200},
+{"id":"虫群近卫","race":"Zerg","level":6,"number":7,"value":2400},
 {"id":"杀心骤起","race":"Neutral","level":6,"number":2,"value":2800}
 ]};
